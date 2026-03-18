94acd89f5bd9b87cc1aa4af3420b3f36d1ae3850
Anuj Savant <anujsavant338@gmail.com>
2026-03-17 20:20:42 -0700

    New Update

--- app.py
+++ app.py
@@ -135,6 +135,4 @@ def logout():
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
     init_db()
-if __name__ == "__main__":
-    init_db()
     app.run(host="0.0.0.0", port=5000)
